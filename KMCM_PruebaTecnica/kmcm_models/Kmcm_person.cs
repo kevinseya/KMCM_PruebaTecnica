68361557b49f27c1f0be77c5e5fa4fa7e4f611f0
kevinseya <kevinseya@gmail.com>
2024-10-03 18:47:53 -0500

    Agregar archivos de proyecto.

---
+++ KMCM_PruebaTecnica/kmcm_models/Kmcm_person.cs
@@ -0,0 +1,12 @@
+﻿namespace KMCM_PruebaTecnica.kmcm_models
+{
+	public class Kmcm_person
+	{
+		public int kmcm_id { get; set; }
+		public string kmcm_name { get; set; }
+		public string kmcm_lastname { get; set; }
+		public string kmcm_address { get; set; }
+		public string kmcm_phone{ get; set; }
+		public DateTime kmcm_birthdate { get; set; }
+	}
+}
---
+++ KMCM_PruebaTecnica/kmcm_models/kmcm_DbContext/Kmcm_DbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using KMCM_PruebaTecnica.kmcm_models;
+
+namespace KMCM_PruebaTecnica.kmcm_model.kmcm_DbContext
+{
+	public partial class Kmcm_DbContext : DbContext
+	{
+		public Kmcm_DbContext(DbContextOptions<Kmcm_DbContext> options)
+		: base(options)
+		{ 
+		}
+
+		public virtual DbSet<Kmcm_person> Persons { get; set; }
+
+	}
+
+}
