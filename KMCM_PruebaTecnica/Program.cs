fa732fb6bc1e8afd5f0d6bd736ec8eb216ae1b74
kevinseya <kevinseya@gmail.com>
2024-10-03 20:53:05 -0500

    Log system update

--- KMCM_PruebaTecnica/Program.cs
+++ KMCM_PruebaTecnica/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Serilog;
 using KMCM_PruebaTecnica.kmcm_models.kmcm_DbContext;
+using KMCM_PruebaTecnica.kmcm_accessData;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,15 +15,27 @@ builder.Services.AddSwaggerGen();
 // Obtener la cadena de conexión desde appsettings.json
 var connectionStringSQLServer = builder.Configuration.GetConnectionString("Kmcm_SqlServer");
 var connectionStringMongo = builder.Configuration.GetConnectionString("Kmcm_MongoDB");
-var collectionStringMongo = builder.Configuration.GetConnectionString("collection");
+var pathString = builder.Configuration.GetConnectionString("Kmcm_PathLog");
 // Agregar el contexto a los servicios
 builder.Services.AddDbContext<Kmcm_DbContext>(options =>
 	options.UseSqlServer(connectionStringSQLServer));
 
+// Agregar el repositorio a los servicios
+builder.Services.AddScoped<kmcm_repositoryPerson>();
+
+
+// Asegurarse de que el directorio de logs existe
+var logDirectory = @"C:\logs";
+if (!Directory.Exists(logDirectory))
+{
+	Directory.CreateDirectory(logDirectory);
+}
+
 // Para escribir logs en la consola
 Log.Logger = new LoggerConfiguration()
-	.WriteTo.Console() 
-	.WriteTo.MongoDB(connectionStringMongo, collectionName: collectionStringMongo) 
+	.WriteTo.Console()
+	.WriteTo.MongoDB(connectionStringMongo)
+	.WriteTo.File(pathString)
 	.CreateLogger();
 
 builder.Host.UseSerilog();
