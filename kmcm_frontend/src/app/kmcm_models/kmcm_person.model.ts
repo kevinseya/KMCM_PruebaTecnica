d670df0747da0abad772cc8ef18e8abf897ac6f0
kevinseya <kevinseya@gmail.com>
2024-10-04 01:36:30 -0500

    Add Component Person and Services Auth and Person

---
+++ kmcm_frontend/src/app/kmcm_models/kmcm_person.model.ts
@@ -0,0 +1,9 @@
+export interface Kmcm_person {
+  kmcm_id?: number | null;
+  kmcm_name: string;
+  kmcm_lastname: string;
+  kmcm_address?: string;
+  kmcm_phone: string;
+  kmcm_birthdate: string;
+}
+
